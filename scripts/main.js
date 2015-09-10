bc1fb6fb1d7a80d68ad7be2149e2b757f926abff
NE05KY <leshka.neo@gmail.com>
2015-09-10 21:50:23 +0300

    Added notes for all slides, changed slide order.

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ function checkSlide() {
     var curr = shower.getCurrentSlideNumber();
 
     // Icons page animation
-    if ( curr === 17 && !iconsAction ) {
+    if ( curr === 13 && !iconsAction ) {
         iconsAction = setInterval( changeRandomIcon, 300 );
     }
-    if ( curr !== 17 && iconsAction ) {
+    if ( curr !== 13 && iconsAction ) {
         window.clearInterval( iconsAction );
         iconsAction = undefined;
     }
